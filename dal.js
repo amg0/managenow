3630d2f2c471170caabff9ae9d5b38b5eaae336c
amg0 <alexis.mermet@gmail.com>
2015-12-28 14:37:34 +0100

    genericdal_v1

--- dal.js
+++ dal.js
@@ -1,87 +1,27 @@
 // module DAL
-var mysql = require("mysql");		// mysql access
 var winston = require("winston");	// logging functionality
-var con = null;
+var dalEmployee = require("./genericdal")('employees');
 
 exports.init = function(callback) {
-	// First you need to create a connection to the db
-	con = mysql.createConnection({
-	  host: "localhost",
-	  user: "amg0",
-	  password: "Clem0tine",
-	  database: "testdb"
-	});
-
-	con.connect(function(err){
-	  if(err){
-		winston.error('Error connecting to Db');
-		return;
-	  } else
-		winston.info('connected as id ' + con.threadId);
-	  (callback)(err);
-	});	
+	dalEmployee.init(callback);
 }
 
 exports.listAllEmployes = function(fields, callback) {
-	var sql = 'SELECT * FROM employees';
-	if (fields && fields.length>0)
-		sql = mysql.format('SELECT ?? FROM employees', [fields]);
-	winston.info('SQL for list all:%s',sql);
-	con.query(sql, function(err,rows){
-	  if(err) throw err;
-	  winston.info('Data received from Db:\n');
-	  winston.info( rows );	//JSON.stringify(rows,null,4)
-	  (callback)(err,rows);
-	});	
+	dalEmployee.listAll(fields, callback);
 }
 
 exports.addEmployee = function( employee , callback ) {
-	delete employee.id;
-	con.query('INSERT INTO employees SET ?', employee, function(err,result){
-	  if(err) throw err;
-	  winston.info('Added => Last insert ID: %d', result.insertId);
-	  winston.info(result);
-	  (callback)(err,result);
-	});	
+	dalEmployee.add(employee , callback);
 }
 
 exports.updateEmployee = function( id, changes, callback  ) {
-	// changes.push(id);
-	con.query(
-	  'UPDATE employees SET ? Where ID = ?',
-	  [changes,id],
-	  function (err, result) {
-		if (err) throw err;
-		winston.info('Changed ' + result.changedRows + ' rows');
-		winston.info(result);
-		(callback)(err,result);
-	  }
-	);
+	dalEmployee.update(id,changes,callback);
 }
 
 exports.deleteEmployee = function( id, callback ) {
-	con.query(
-	  'DELETE FROM employees WHERE id = ?',
-	  [id],
-	  function (err, result) {
-		if (err) throw err;
-		winston.info('Deleted ' + result.affectedRows + ' rows');
-		winston.info(result);
-		(callback)(err,result);
-	  }
-	);
+	dalEmployee.destroy(id,callback);
 }
 
 exports.exit = function( callback ) {
-	con.end(function(err) {
-		if(err){
-			winston.error('Error happened while disconnecting from Db');
-		} else {
-			winston.info('The connection is terminated gracefully');
-			// Ensures all previously enqueued queries are still
-			// before sending a COM_QUIT packet to the MySQL server.
-		}
-		con=null;
-		(callback)(err);
-	});
+	dalEmployee.exit(callback);
 }
--- genericdal.js
+++ genericdal.js
@@ -1,87 +1,89 @@
 // module DAL
 var mysql = require("mysql");		// mysql access
 var winston = require("winston");	// logging functionality
-var con = null;
+var _tablename = null;
 
-exports.init = function(callback) {
-	// First you need to create a connection to the db
-	con = mysql.createConnection({
-	  host: "localhost",
-	  user: "amg0",
-	  password: "Clem0tine",
-	  database: "testdb"
-	});
+module.exports = function(tablename) {
+	_tablename = tablename;
+	this.con = null;
+	
+	return {
+		init : function(callback) {
+			winston.info("initialize GenericDal for table %s",_tablename);
+			// First you need to create a connection to the db
+			con = mysql.createConnection({
+			  host: "localhost",
+			  user: "amg0",
+			  password: "Clem0tine",
+			  database: "testdb"
+			});
 
-	con.connect(function(err){
-	  if(err){
-		winston.error('Error connecting to Db');
-		return;
-	  } else
-		winston.info('connected as id ' + con.threadId);
-	  (callback)(err);
-	});	
-}
-
-exports.listAllEmployes = function(fields, callback) {
-	var sql = 'SELECT * FROM employees';
-	if (fields && fields.length>0)
-		sql = mysql.format('SELECT ?? FROM employees', [fields]);
-	winston.info('SQL for list all:%s',sql);
-	con.query(sql, function(err,rows){
-	  if(err) throw err;
-	  winston.info('Data received from Db:\n');
-	  winston.info( rows );	//JSON.stringify(rows,null,4)
-	  (callback)(err,rows);
-	});	
-}
-
-exports.addEmployee = function( employee , callback ) {
-	delete employee.id;
-	con.query('INSERT INTO employees SET ?', employee, function(err,result){
-	  if(err) throw err;
-	  winston.info('Added => Last insert ID: %d', result.insertId);
-	  winston.info(result);
-	  (callback)(err,result);
-	});	
-}
-
-exports.updateEmployee = function( id, changes, callback  ) {
-	// changes.push(id);
-	con.query(
-	  'UPDATE employees SET ? Where ID = ?',
-	  [changes,id],
-	  function (err, result) {
-		if (err) throw err;
-		winston.info('Changed ' + result.changedRows + ' rows');
-		winston.info(result);
-		(callback)(err,result);
-	  }
-	);
-}
-
-exports.deleteEmployee = function( id, callback ) {
-	con.query(
-	  'DELETE FROM employees WHERE id = ?',
-	  [id],
-	  function (err, result) {
-		if (err) throw err;
-		winston.info('Deleted ' + result.affectedRows + ' rows');
-		winston.info(result);
-		(callback)(err,result);
-	  }
-	);
-}
-
-exports.exit = function( callback ) {
-	con.end(function(err) {
-		if(err){
-			winston.error('Error happened while disconnecting from Db');
-		} else {
-			winston.info('The connection is terminated gracefully');
-			// Ensures all previously enqueued queries are still
-			// before sending a COM_QUIT packet to the MySQL server.
+			con.connect(function(err){
+			  if(err){
+				winston.error('Error connecting to Db');
+				return;
+			  } else
+				winston.info('connected as id ' + con.threadId);
+			  (callback)(err);
+			});	
+		},
+		exit : function( callback ) {
+			con.end(function(err) {
+				if(err){
+					winston.error('Error happened while disconnecting from Db');
+				} else {
+					winston.info('The connection is terminated gracefully');
+					// Ensures all previously enqueued queries are still
+					// before sending a COM_QUIT packet to the MySQL server.
+				}
+				con=null;
+				(callback)(err);
+			});
+		},
+		listAll : function(fields,callback) {
+			var sql = 'SELECT * FROM '+_tablename;
+			if (fields && fields.length>0)
+				sql = mysql.format('SELECT ?? FROM '+_tablename, [fields]);
+			winston.info('SQL for list all:%s',sql);
+			con.query(sql, function(err,rows){
+			  if(err) throw err;
+			  winston.info('Data received from Db:');
+			  winston.info( rows );	//JSON.stringify(rows,null,4)
+			  (callback)(err,rows);
+			});					
+		},
+		add : function( object , callback ) {
+			delete object.id;
+			con.query('INSERT INTO '+_tablename+' SET ?', object, function(err,result){
+			  if(err) throw err;
+			  winston.info('Added => Last insert ID: %d', result.insertId);
+			  winston.info(result);
+			  (callback)(err,result);
+			});	
+		},
+		update : function( id, object, callback  ) {
+			con.query(
+			  'UPDATE '+_tablename+' SET ? Where ID = ?',
+			  [object,id],
+			  function (err, result) {
+				if (err) throw err;
+				winston.info('Changed ' + result.changedRows + ' rows');
+				winston.info(result);
+				(callback)(err,result);
+			  }
+			);
+		},
+		destroy : function( id, callback ) {
+			con.query(
+			  'DELETE FROM '+_tablename+' WHERE id = ?',
+			  [id],
+			  function (err, result) {
+				if (err) throw err;
+				winston.info('Deleted ' + result.affectedRows + ' rows');
+				winston.info(result);
+				(callback)(err,result);
+			  }
+			);
 		}
-		con=null;
-		(callback)(err);
-	});
-}
+	};
+};
